45a4e97e701baeccc1cf7c77b6a997627faf8ede
IvanSobolev <i@bezlikiy.ru>
2025-03-24 23:44:00 +0300

    Mini fix player

--- ComputerClub/Model/Entitys/Player.cs
+++ ComputerClub/Model/Entitys/Player.cs
@@ -4,7 +4,7 @@ public class Player
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public List<string> GamesName { get; set; } //Честно, по логике лучше сделать отдельную энтити с играми, ибо так может быть дублирование данных 🤷‍♂️
+    public List<string> GamesName { get; set; } //По логике лучше сделать отдельную энтити с играми, ибо так может быть дублирование данных 🤷‍♂️
     public List<int> SessionsId { get; set; }
     public int Year { get; set; }
     
