0bf5c9bf0d5e5d9d10cd73be78812715d27936fa
IvanSobolev <i@bezlikiy.ru>
2025-03-24 21:11:52 +0300

    feat: players entity

---
+++ ComputerClub/Model/Entitys/Players.cs
@@ -0,0 +1,10 @@
+﻿namespace ComputerClub.Model.Entitys;
+
+public class Players
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<string> GamesName { get; set; } //Честно, по логике лучше сделать отдельную энтити с играми, ибо так может быть дублирование данных 🤷‍♂️
+    public List<int> SessionsId { get; set; }
+    public int Year { get; set; }
+}
