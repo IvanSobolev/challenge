0b6e155c52d07ba5e9b9d952724237d3c950cca9
IvanSobolev <i@bezlikiy.ru>
2025-03-24 23:44:44 +0300

    feat: Implementation repository

---
+++ ComputerClub/Repository/Implementation/EfCorePlayerRepository.cs
@@ -0,0 +1,34 @@
+﻿using ComputerClub.Model;
+using ComputerClub.Model.Entitys;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerClub.Repository.Implementation;
+
+public class EfCorePlayerRepository(DataContext dataContext) : IPlayerRepository
+{
+    private readonly DataContext _dataContext = dataContext;
+    
+    public async Task AddUserAsync(string name, int year)
+    {
+        Player newPlayer = new Player()
+        {
+            Name = name,
+            GamesName = new List<string>(),
+            SessionsId = new List<int>(),
+            Year = year
+        };
+        await _dataContext.Players.AddAsync(newPlayer);
+        await _dataContext.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<string>> GetAllUserGamesAsync(int userId)
+    {
+        Player? player = await _dataContext.Players.FirstOrDefaultAsync(u => u.Id == userId);
+        if (player == null)
+        {
+            return new List<string>();
+        }
+
+        return player.GamesName;
+    }
+}
---
+++ ComputerClub/Repository/Implementation/EfCoreSessionRepository.cs
@@ -0,0 +1,15 @@
+﻿using ComputerClub.Model;
+using ComputerClub.Model.Entitys;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerClub.Repository.Implementation;
+
+public class EfCoreSessionRepository(DataContext dataContext) : ISessionRepository
+{
+    private readonly DataContext _dataContext = dataContext;
+    
+    public async Task<float> GetAvgPlayerPlaytime(int playerId)
+    {
+        return (float)await _dataContext.Sessions.Where(s => s.PlayerId == playerId).AverageAsync(s => s.DurationMinutes);
+    }
+}
