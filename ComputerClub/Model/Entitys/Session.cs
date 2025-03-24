b46a6dd58909eea9423264ecad85997b333653a3
IvanSobolev <i@bezlikiy.ru>
2025-03-24 21:21:00 +0300

    feat: session entity

---
+++ ComputerClub/Model/Entitys/Session.cs
@@ -0,0 +1,10 @@
+﻿namespace ComputerClub.Model.Entitys;
+
+public class Session
+{
+    public int Id { get; set; }
+    public string GameName { get; set; }
+    public DateTime StartTime { get; set; }
+    public int DurationMinutes { get; set; }
+    public int PlayerId { get; set; }
+}
