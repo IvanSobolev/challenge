1a9d5f701a34913c3e61b1ef04f5fd1fab354e88
IvanSobolev <i@bezlikiy.ru>
2025-03-25 18:30:12 +0300

    feat: Player controller

---
+++ ComputerClub/Controller/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using ComputerClub.Manager.Interfaces;
+using ComputerClub.Model.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerClub.Controller;
+
+[ApiController]
+[Route("players")]
+public class PlayerController(IPlayerManager playerManager) : ControllerBase
+{
+    private readonly IPlayerManager _playerManager = playerManager;
+
+    [HttpPost("add")]
+    public async Task<IActionResult> AddUserAsync([FromBody] AddPlayerDto playerDto)
+    {
+        try
+        {
+            return Ok(await _playerManager.AddUserAsync(playerDto));
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    
+    [HttpGet("GetAllUserGames/{userId}")]
+    public async Task<IActionResult> GetAllUserGamesAsync(int userId)
+    {
+        try
+        {
+            return Ok(await _playerManager.GetAllUserGamesAsync(userId));
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
