f7846b57ea4562544ad7724cf60bded33963d685
IvanSobolev <i@bezlikiy.ru>
2025-03-25 18:16:09 +0300

    feat: manager implementation

---
+++ ComputerClub/Manager/Implementation/SessionManager.cs
@@ -0,0 +1,15 @@
+﻿using ComputerClub.Manager.Interfaces;
+using ComputerClub.Repository.Implementation;
+
+namespace ComputerClub.Manager.Implementation;
+
+public class SessionManager(ISessionRepository sessionRepository) : ISessionManager
+{
+    private readonly ISessionRepository _sessionRepository = sessionRepository;
+    
+    /// <inheritdoc />
+    public async Task<float> GetAvgPlayerPlaytime(int playerId)
+    {
+        return await _sessionRepository.GetAvgPlayerPlaytime(playerId);
+    }
+}
---
+++ ComputerClub/Manager/Implementation/SubscriptionManager.cs
@@ -0,0 +1,22 @@
+﻿using ComputerClub.Manager.Interfaces;
+using ComputerClub.Model.DTO;
+using ComputerClub.Model.Entitys;
+using ComputerClub.Repository.Implementation;
+
+namespace ComputerClub.Manager.Implementation;
+
+public class SubscriptionManager(ISubscriptionRepository subscriptionRepository) : ISubscriptionManager
+{
+    private readonly ISubscriptionRepository _subscriptionRepository = subscriptionRepository;
+    
+    public async Task<Subscription> AddSubscrioption(AddSubsciptionDto subsciptionDto)
+    {
+        return await _subscriptionRepository.AddSubscrioption(subsciptionDto.PlayerId, subsciptionDto.EndDate,
+            subsciptionDto.Balance);
+    }
+
+    public async Task DeleteSubscription(int subId)
+    {
+        await _subscriptionRepository.DeleteSubscription(subId);
+    }
+}
